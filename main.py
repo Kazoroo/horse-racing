1aa169db41885e9e2e3c36497f519e2179c6b6ad
Kazoroo <91783342+Kazoroo@users.noreply.github.com>
2025-02-07 13:34:31 +0100

    Use elif instead of else-if-if-if

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 import time
 import random
 
-
 horse = random.randint(1, 13)
 position = random.randint(2, 13)
-
 
 bet = int(input("Postaw zakład: "))
 
@@ -16,16 +14,13 @@ time.sleep(1.5)
 print("Trwa wyścig...")
 time.sleep(3)
 
-
-# wynik wyścigu
 if user_choice == horse:
     award = bet * 3
     print(f"Twój koń skończył na 1. miejscu, wygrałeś! {award} trafia na twoje konto")
+elif position == 2:
+    award = bet * 1.5
+elif position == 3:
+    award = bet * 1.25
 else:
-    if position == 2:
-        award = bet * 1.5
-    if position == 3:
-        award = bet * 1.25
-    if position > 3:
-        award = 0
-    print(f"Przegrałeś, twój koń skończył na {position}. miejscu. Otrzymałeś {award} pieniądzy postawionych na zakład")
+    award = 0
+print(f"Przegrałeś, twój koń skończył na {position}. miejscu. Otrzymałeś {award} pieniądzy postawionych na zakład")
